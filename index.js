174a8f68a9fde489fbb5f6c3045afec62f2b2385
Kunal Shrestha <kunalshrestha377@gmail.com>
2026-02-04 18:23:22 +0545

    done

--- index.js
+++ index.js
@@ -17,8 +17,11 @@ app.use(
     origin: [
       "http://localhost:3000",
       "http://localhost:3001",
-      "https://frontend-project-awth.vercel.app",
+      "https://astounding-sunburst-cbc9bb.netlify.app",
     ],
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
   }),
 );
 
