a39e8cfa94792c32afefd8f534d4e1d76d75ff5b
Kunal Shrestha <kunalshrestha377@gmail.com>
2026-02-04 17:06:37 +0545

    done commit

--- index.js
+++ index.js
@@ -4,12 +4,12 @@ import { userController } from "./user/user.controller.js";
 import { productController } from "./product/product.controller.js";
 import { cartController } from "./cart/cart.controller.js";
 import cors from "cors";
+import dotenv from "dotenv";
 
 const app = express();
 
 app.use(express.json());
 
-import dotenv from "dotenv";
 dotenv.config();
 
 app.use(
@@ -28,6 +28,7 @@ app.use(userController);
 app.use(productController);
 app.use(cartController);
 
-app.listen(8000, () => {
-  console.log("App is listening on port 8000");
+const PORT = process.env.PORT || 8000;
+app.listen(PORT, () => {
+  console.log(`App is listening on port ${PORT}`);
 });
